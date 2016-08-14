521ce9fe220c9703b774ac6564b6fe04fa2078b2
dumosin <dumosin@sina.com>
2016-08-14 11:15:05 -0400

    Database address and matedata

---
+++ MySQL_Graph.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import MySQLdb
+import pandas as pd
+import plotly.plotly as py
+from plotly.graph_objs import *
+py.sign_in("dumosin","Iamhacker1992!")
+
+conn = MySQLdb.connect(host="localhost", user="root", passwd="123456", db="world")
+cursor = conn.cursor()
+
+sql = "select Name, Continent, Population, LifeExpectancy, GNP from Country"
+cursor.execute(sql)
+rows = cursor.fetchall()
+str(rows[0:300])
+
+df = pd.DataFrame([[ij for ij in i] for i in rows])
+df.rename(columns={0: 'Name', 1: 'Continent', 2: 'Population', 3: 'LifeExpectancy', 4: 'GNP'}, inplace=True)
+df = df.sort_values(['LifeExpectancy'], ascending=[1])
+df.head()
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-mysql
 Reading data from a MySQL database and graphing it in Python
+
+MySQL example database:http://downloads.mysql.com/docs/world.sql.zip

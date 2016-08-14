f46cf6c1aae1c12299a86870100dfe35b445ace2
dumosin <dumosin@sina.com>
2016-08-14 23:16:36 +0800

    Update MySQL_Graph.py

--- MySQL_Graph.py
+++ MySQL_Graph.py
@@ -4,7 +4,7 @@ import MySQLdb
 import pandas as pd
 import plotly.plotly as py
 from plotly.graph_objs import *
-py.sign_in("dumosin","Iamhacker1992!")
+py.sign_in("dumosin","******")
 
 conn = MySQLdb.connect(host="localhost", user="root", passwd="123456", db="world")
 cursor = conn.cursor()
